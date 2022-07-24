2ddce0717f21efeacc776cef2d6315d642d1f6a9
Sanjay Namdeo <sanjay.namdeo@live.com>
2022-07-24 09:45:53 +0000

    Added JetService and Filter

---
+++ src/main/java/com/learn/security/springsecuritywithjwt/filter/JwtFilter.java
@@ -0,0 +1,51 @@
+package com.learn.security.springsecuritywithjwt.filter;
+
+import com.learn.security.springsecuritywithjwt.service.JwtUtilService;
+import com.learn.security.springsecuritywithjwt.service.UserDetailServiceImpl;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    public JwtUtilService jwtUtilService;
+    public UserDetailServiceImpl userDetailService;
+
+    public JwtFilter(JwtUtilService jwtUtilService, UserDetailServiceImpl userDetailService) {
+        this.jwtUtilService = jwtUtilService;
+        this.userDetailService = userDetailService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+
+        if (authorization != null && authorization.startsWith("Bearer")) {
+            token = authorization.substring(7);
+            username = jwtUtilService.extractUsername(token);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailService.loadUserByUsername(username);
+            if (jwtUtilService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/learn/security/springsecuritywithjwt/service/JwtUtilService.java
@@ -0,0 +1,65 @@
+package com.learn.security.springsecuritywithjwt.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtUtilService {
+    @Value("${jwt.expiration.time}")
+    public int jwtExpiry;
+    SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String createToken(Map<String, Object> claim, String subject) {
+        Date expirationTime = new Date(new Date().getTime() + jwtExpiry);
+
+        return Jwts.builder()
+                .setClaims(claim)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(expirationTime)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateToken(final String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+}
