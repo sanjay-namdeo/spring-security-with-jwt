94c6b12030a596fb4c7b4e162c0d95c9c989fbab
Sanjay Namdeo <sanjay.namdeo@live.com>
2022-07-25 15:15:59 +0000

    Refactored and optimized JwtFilter

--- src/main/java/com/learn/security/springsecuritywithjwt/advice/MainExceptionHandler.java
+++ src/main/java/com/learn/security/springsecuritywithjwt/advice/MainExceptionHandler.java
@@ -2,6 +2,7 @@ package com.learn.security.springsecuritywithjwt.advice;
 
 import com.learn.security.springsecuritywithjwt.exception.ErrorResponse;
 import com.learn.security.springsecuritywithjwt.exception.IncorrectUsernameOrPasswordException;
+import com.learn.security.springsecuritywithjwt.exception.InvalidJWTException;
 import com.learn.security.springsecuritywithjwt.exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,19 +21,25 @@ public class MainExceptionHandler extends ResponseEntityExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Object> handleBadRequestException(UserNotFoundException exception) {
         log.info("User is not available!");
-        return buildErrorResponse(exception, exception.getMessage(), exception.getStatus());
+        return buildErrorResponse(exception, exception.getStatus());
     }
 
     @ExceptionHandler(IncorrectUsernameOrPasswordException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    private ResponseEntity<Object> handleIncorrectUserOrPassword(IncorrectUsernameOrPasswordException exception) {
+    public ResponseEntity<Object> handleIncorrectUserOrPassword(IncorrectUsernameOrPasswordException exception) {
         log.info("Username or password is wrong!");
-        return buildErrorResponse(exception, exception.getMessage(), exception.getStatus());
+        return buildErrorResponse(exception, exception.getStatus());
     }
 
-    private ResponseEntity<Object> buildErrorResponse(Exception ex, String message, HttpStatus status) {
+    public ResponseEntity<Object> buildErrorResponse(Exception ex, HttpStatus status) {
         ErrorResponse errorResponse = new ErrorResponse(status.value(), ex.getMessage(), LocalDateTime.now());
         log.info("ErrorResponse built for error: " + errorResponse.getMessage());
         return ResponseEntity.status(status).body(errorResponse);
+    }
+
+    @ExceptionHandler(InvalidJWTException.class)
+    public ResponseEntity<Object> handleInvalidJWTToken(InvalidJWTException exception) {
+        log.error("Invalid JWT token");
+        return buildErrorResponse(exception, exception.getStatus());
     }
 }
